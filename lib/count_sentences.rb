a52e1fdc9cedffeeb0a47b7009d5af642492ebe0
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-01 04:14:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.') ? true : false 
   end
 
   def question?
-
+    self.end_with?('?') ? true : false 
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false 
   end
 
-  def count_sentences
-
+   def count_sentences
+    divide_string =self.split(/\w[.?!]\s[A-z]|\w[.?!]\s\z|\w[.?!][..]/)
+    
+    divide_string.size
+    
   end
 end
