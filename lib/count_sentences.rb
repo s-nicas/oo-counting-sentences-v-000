d7ea5c8c9d9daec931562024936b90a95df8e155
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-01 03:28:57 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,16 +4,14 @@ class String
 
   def sentence?
     self.end_with?('.') ? true : false 
-    
   end
 
   def question?
     self.end_with?('?') ? true : false 
-
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false 
   end
 
   def count_sentences
